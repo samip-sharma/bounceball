59296baa53848e0f72225015afceb8ac85dec44c
github username <github email address>
2019-08-13 16:28:19 -0400

    canvas setup done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 import Ball from './Ball'
 
-function App() {
+class App extends React.Component{
+
+state = {
+    ball: {
+        x: 200,
+        y: 50,
+        radius: 10
+    }
+}
+
+draw = () => {
+  const ctx = this.refs.canvas.getContext("2d");
+  // change this colour to change the colour of your 
+  // "pen" in the canvas 
+  ctx.fillStyle = "green";
+  ctx.beginPath();
+  ctx.arc(this.state.ball.x, this.state.ball.y,                        
+          this.state.ball.radius, 0, 2 * Math.PI);
+  ctx.fill();
+  ctx.stroke();
+}
+
+  componentDidMount() {
+    this.draw()
+  }
+
+  render(){
   return (
-    <div height="500px" width="500px" className="App">
-      
-    </div>
-  );
+    <div>
+        <canvas ref="canvas" width={450} height={650} className="App" ></canvas>
+        </div>
+  ) 
+}
 }
 
 export default App;
--- src/Ball.js
+++ src/Ball.js
@@ -1,10 +1,23 @@
 import React from 'react' 
 
 export default class Ball extends React.Component{
+
+    state={
+        x:0,
+        y:0
+    }
+
+    handleJump=(e)=>{
+        let now=this
+        debugger
+        // console.log(e.target.style.bottom)
+        console.log(e.target.style.right)
+        // e.style.bottom = "100px";
+    }
     render(){
         return(
             <React.Fragment>
-
+        <img   onClick={this.handleJump} className="small-ball" src="https://media.istockphoto.com/photos/tennis-ball-picture-id137345149?k=6&m=137345149&s=612x612&w=0&h=3DwFtXaN83o-VBiD4fwJK_fmuilNForlOylwRCb0Low="></img>
             </React.Fragment>
         )
     }
